e171b6bad59b72b6e509db23753e9dafd7b07b2e
ilmanuser <email@example.com>
2024-10-29 16:35:53 +0300

    add commit for string Hello

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("Hello World!");
+
         boolean isActive = true;
         Boolean isActive2 = false;
 
